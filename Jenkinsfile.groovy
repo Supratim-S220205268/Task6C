209df859defdf741ac94e6a980c4e8fb3fa2487b
Supratim-S220205268 <168605647+Supratim-S220205268@users.noreply.github.com>
2024-05-10 20:43:50 +1000

    Add JenkinsFile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,90 @@
+pipeline{
+    
+    agent any
+    
+    stages{
+        
+        stage('Build') {
+            steps {
+                echo "Building the code using a build automation tool to compile and package the code."
+                echo "Tool Used: Apache Ant"
+            }
+        }
+        stage('Unit and Integration Tests') {
+            steps {
+                echo "Running the unit tests to ensure the code functions as expected and running integration tests to ensure the different components of the application work together as expected."
+                echo "Tool Used: Selenium"
+            }
+             post {
+                success{
+                    mail to: "s220205268@deakin.edu.au",
+                    subject: "Unit and Integration Test Alert",
+                    body: "Status: The Unit and Integration Testing is completed successfully."
+                    
+                }
+                failure{
+                    mail to: "s2202052668@deakin.edu.au",
+                    subject: "Unit and Integration Test Alert",
+                    body: "Status: The Unit and Integration Testing has failed."
+                }
+            }
+        }
+        stage('Code Analysis') {
+            steps {
+                echo "Integrating code analysis tool to analyse the code and ensure it meets industry standards."
+                echo "Tool Used: SonarQube"
+            }
+        }
+        stage('Security Scan') {
+            steps {
+                echo "Performing a security scan on the code using a tool to identify any vulnerabilities."
+                echo "Tool Used: Nessus"
+            }
+            post {
+                success{
+                    mail to: "s2202052668@deakin.edu.au",
+                    subject: "Security Scan Alert",
+                    body: "Status: The Security Scan has been completed successfully"
+                }
+                failure{
+                    mail to: "s2202052668@deakin.edu.au",
+                    subject: "Security Scan Alert",
+                    body: "Status: The Security Scan has failed."
+                }
+            }
+        }
+        stage('Deploy to Staging') {
+            steps {
+                echo "Deploying the application to a staging server"
+                echo "Tool Used: Bamboo"
+            }
+        }
+        stage('Integration Tests on Staging') {
+            steps {
+                echo "Running integration tests on the staging environment to ensure the application functions as expected in a production-like environment."
+                echo "Tool Used: Citrus"
+            }
+            post {
+                success{
+                    mail to: "s220205268@deakin.edu.au",
+                    subject: "Staging - Integration Test Alert",
+                    body: "Status: The Integration Testing on Staging environment is completed successfully."
+                    
+                }
+                failure{
+                    mail to: "s2202052668@deakin.edu.au",
+                    subject: "Staging - Integration Test Alert",
+                    body: "Status: The Integration Testing on Staging environment has failed."
+                }
+            }
+        }
+        stage('Deploy to Production') {
+            steps {
+                echo "Deploying the application to a production server"
+                echo "Tool Used: CircleCI"
+            }
+        }
+        
+        
+    }
+}
